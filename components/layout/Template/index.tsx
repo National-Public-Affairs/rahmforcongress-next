4b45cef25519edee6ec1d125249a5799368d0daa
asb1512 <andrew@olympicmedia.com>
2023-05-28 15:23:38 -0400

    rendering footer

--- components/layout/Template/index.tsx
+++ components/layout/Template/index.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
+import Footer from '../Footer';
+import { FooterType, LegalType, SocialType } from '@/types/globals';
 import classes from './styles.module.scss';
 
 type Props = {
+  footer: FooterType;
+  legal?: LegalType;
+  socialMedia?: SocialType;
   children: any;
 }
 
 const Template: React.FC<Props> = ({
+  footer,
+  socialMedia,
+  legal,
   children,
 }) => (
   <>
     <div className={classes.wrapper}>
       {children}
     </div>
-    {/* footer goes here */}
+    <Footer
+      footer={footer}
+      socialMedia={socialMedia}
+      legal={legal}
+    />
   </>
 );
 
