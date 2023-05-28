0734ea2b4210dd709c005fa590094d8914865644
asb1512 <andrew@olympicmedia.com>
2023-05-28 18:45:41 -0400

    implicit return

--- components/RenderBlocks/index.tsx
+++ components/RenderBlocks/index.tsx
@@ -7,36 +7,32 @@ type Props = {
   className?: string;
 }
 
-const RenderBlocks: React.FC<Props> = ({ layout, className }) => {
-  console.log('lay your ass out', layout)
-
-  return (
-    <div className={[
-      className,
-    ].filter(Boolean).join(' ')}
-    >
-      {
-        layout.length > 0 && layout.map((block) => {
-          switch (block.blockType) {
-            case 'content':
-              break;
-            case 'donation':
-              return (
-                <section>
-                  <DonationBlock
-                    backgroundColor={block.backgroundColor}
-                    cta={block.cta}
-                    options={block.options}
-                  />
-                </section>
-              );
-            default:
-              break;
-          }
-        })
-      }
-    </div>
-  );
-}
+const RenderBlocks: React.FC<Props> = ({ layout, className }) => (
+  <div className={[
+    className,
+  ].filter(Boolean).join(' ')}
+  >
+    {
+      layout.length > 0 && layout.map((block) => {
+        switch (block.blockType) {
+          case 'content':
+            break;
+          case 'donation':
+            return (
+              <section>
+                <DonationBlock
+                  backgroundColor={block.backgroundColor}
+                  cta={block.cta}
+                  options={block.options}
+                />
+              </section>
+            );
+          default:
+            break;
+        }
+      })
+    }
+  </div>
+);
 
 export default RenderBlocks;
