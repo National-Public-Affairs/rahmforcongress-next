7b6695883c6150c4b766a171f01807f13268ee13
asb1512 <andrew@olympicmedia.com>
2023-05-28 12:22:12 -0400

    turned to JSX file

---
+++ components/RichText/serialize.jsx
@@ -0,0 +1,141 @@
+import React, { Fragment } from 'react';
+import escapeHTML from 'escape-html';
+import { Text } from 'slate';
+import { colors } from '@/styles/styles';
+import classes from './styles.module.scss';
+import typestyles from '../../styles/app.module.scss';
+
+// type Children = Leaf[]
+
+// type Leaf = {
+//   type: string;
+//   value?: {
+//     url: string;
+//     alt: string;
+//   };
+//   children?: Children;
+//   url?: string;
+//   [key: string]: unknown;
+// }
+
+// const serialize = (children: Children): React.ReactElement[] => children?.map((node, i) => {
+  const serialize = (children) =>
+    children?.map((node, i) => {
+      if (Text.isText(node)) {
+        let text = (
+          <span dangerouslySetInnerHTML={{ __html: escapeHTML(node.text) }} />
+        );
+
+        if (node.bold) {
+          text = <strong key={i}>{text}</strong>;
+        }
+
+        // custom styling for purple text
+        if (node['rich-purple-text']) {
+          text = <div style={{ color: colors.purple }}>{text}</div>;
+        }
+
+        // custom styling for dark purple text
+        if (node['rich-dark-purple-text']) {
+          text = <div style={{ color: colors.darkPurple }}>{text}</div>;
+        }
+
+        // custom styling for yellow text
+        if (node['rich-yellow-text']) {
+          text = <div style={{ color: colors.yellow }}>{text}</div>;
+        }
+
+        // custom styling for white text
+        if (node['rich-white-text']) {
+          text = <div style={{ color: colors.white }}>{text}</div>;
+        }
+
+        // custom styling for yellow headline text
+        if (node['rich-yellow-headline']) {
+          text = (
+            <div
+              style={{ color: colors.yellow }}
+              className={`${typestyles.h1} ${classes.headline}`}
+            >
+              {text}
+            </div>
+          );
+        }
+
+        // custom styling for white headline text
+        if (node['rich-white-headline']) {
+          text = (
+            <div 
+              style={{ color: colors.white }}
+              className={`${typestyles.h1} ${classes.headline}`}
+            >
+              {text}
+            </div>
+          );
+        }
+
+        if (node.code) {
+          text = <code key={i}>{text}</code>;
+        }
+
+        if (node.italic) {
+          text = <em key={i}>{text}</em>;
+        }
+
+        if (node.underline) {
+          text = (
+            <span style={{ textDecoration: 'underline' }} key={i}>
+              {text}
+            </span>
+          );
+        }
+
+        if (node.strikethrough) {
+          text = (
+            <span style={{ textDecoration: 'line-through' }} key={i}>
+              {text}
+            </span>
+          );
+        }
+
+        return <Fragment key={i}>{text}</Fragment>;
+      }
+
+      if (!node) {
+        return null;
+      }
+
+      switch (node.type) {
+        case 'h1':
+          return <h1 key={i}>{serialize(node.children)}</h1>;
+        case 'h2':
+          return <h2 key={i}>{serialize(node.children)}</h2>;
+        case 'h3':
+          return <h3 key={i}>{serialize(node.children)}</h3>;
+        case 'h4':
+          return <h4 key={i}>{serialize(node.children)}</h4>;
+        case 'h5':
+          return <h5 key={i}>{serialize(node.children)}</h5>;
+        case 'h6':
+          return <h6 key={i}>{serialize(node.children)}</h6>;
+        case 'quote':
+          return <blockquote key={i}>{serialize(node.children)}</blockquote>;
+        case 'ul':
+          return <ul key={i}>{serialize(node.children)}</ul>;
+        case 'ol':
+          return <ol key={i}>{serialize(node.children)}</ol>;
+        case 'li':
+          return <li key={i}>{serialize(node.children)}</li>;
+        case 'link':
+          return (
+            <a href={escapeHTML(node.url)} key={i}>
+              {serialize(node.children)}
+            </a>
+          );
+
+        default:
+          return <div key={i}>{serialize(node.children)}</div>;
+      }
+    });
+
+export default serialize;
