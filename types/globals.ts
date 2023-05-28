5a4bb850b954532d5ee206d967e280ee6a9d9f38
asb1512 <andrew@olympicmedia.com>
2023-05-28 13:30:49 -0400

    created global types

--- types/globals.ts
+++ types/globals.ts
@@ -5,7 +5,22 @@ export type HeaderType = {
   navItems: NavItemType[];
 }
 
+export type FooterType = {
+  globalType: 'footer';
+  navItems: NavItemType[];
+  displaySocial: 'yes' | 'no';
+  displayCopyright: 'yes' | 'no';
+  displayDisclaimer: 'yes' | 'no';
+}
+
 export type SocialType = {
   globalType: 'social-media';
   links: SocialLinkType[];
+}
+
+export type LegalType = {
+  globalType: 'legal';
+  disclaimer?: string;
+  address?: string;
+  copyright?: string;
 }
