23d6363535089bacf08031f4e09261de56885d60
asb1512 <andrew@olympicmedia.com>
2023-05-28 20:37:11 -0400

    added styling to headings from RTE

--- components/RichText/serialize.jsx
+++ components/RichText/serialize.jsx
@@ -117,17 +117,41 @@ const serialize = (children) =>
 
     switch (node.type) {
       case 'h1':
-        return <h1 key={i}>{serialize(node.children)}</h1>;
+        return (
+          <h1 key={i} className={typestyles.h1}>
+            {serialize(node.children)}
+          </h1>
+        );
       case 'h2':
-        return <h2 key={i}>{serialize(node.children)}</h2>;
+        return (
+          <h2 key={i} className={typestyles.h2}>
+            {serialize(node.children)}
+          </h2>
+        );
       case 'h3':
-        return <h3 key={i}>{serialize(node.children)}</h3>;
+        return (
+          <h3 key={i} className={typestyles.h3}>
+            {serialize(node.children)}
+          </h3>
+        );
       case 'h4':
-        return <h4 key={i}>{serialize(node.children)}</h4>;
+        return (
+          <h4 key={i} className={typestyles.h4}>
+            {serialize(node.children)}
+          </h4>
+        );
       case 'h5':
-        return <h5 key={i}>{serialize(node.children)}</h5>;
+        return (
+          <h5 key={i} className={typestyles.h5}>
+            {serialize(node.children)}
+          </h5>
+        );
       case 'h6':
-        return <h6 key={i}>{serialize(node.children)}</h6>;
+        return (
+          <h6 key={i} className={typestyles.h6}>
+            {serialize(node.children)}
+          </h6>
+        );
       case 'quote':
         return <blockquote key={i}>{serialize(node.children)}</blockquote>;
       case 'ul':
