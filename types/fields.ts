747f3d466a7860dbb486bd9a4be7bdd1bd83c374
asb1512 <andrew@olympicmedia.com>
2023-05-28 20:36:55 -0400

    formatting

--- types/fields.ts
+++ types/fields.ts
@@ -1,39 +1,54 @@
+///////////////////////
+// Nav & Hyperlinks
+///////////////////////
 export type LinkType = {
-  type: "reference" | "custom";
+  type: 'reference' | 'custom';
   reference?: {
     value: {
       slug: string;
-    },
+    };
   };
   label: string;
   newTab?: boolean;
   url?: string;
-}
+};
 
 export type NavItemType = {
   id: string;
   link: LinkType;
-}
+};
 
+///////////////////////
+// Social Media
+///////////////////////
 export type SocialLinkType = {
   id: string;
   url: string;
   label: string;
-}
+};
 
+///////////////////////
+// Rich Text
+///////////////////////
 export type MediaType = {
   id?: string;
   alt?: string;
   mimeType: string;
   url: string;
-}
+};
 
+///////////////////////
+// Meta & SEO
+///////////////////////
 export type MetaType = {
   title?: string;
   description?: string;
   image?: MediaType;
-}
+};
 
+///////////////////////
+// Hero
+///////////////////////
 export type HeroType = {
   type: 'withMedia' | 'minimal';
   richText: any;
@@ -44,4 +59,13 @@ export type HeroType = {
 export type MinimalHeroType = {
   type: 'minimal';
   richText: any;
-}
+};
+
+///////////////////////
+// Rich Text
+///////////////////////
+
+export type RichTextType = {
+  children: any;
+  type: any;
+};
