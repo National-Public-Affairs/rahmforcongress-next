8c6812f18a6e9ca514a0a0d321f3dfc84fa7560c
asb1512 <asb1512@gmail.com>
2023-05-31 17:55:04 -0400

    added grid and cell

--- components/layout/PageHero/Minimal/index.tsx
+++ components/layout/PageHero/Minimal/index.tsx
@@ -5,6 +5,7 @@ import GridContainer from '../../GridContainer';
 import PolygonTwo from '@/components/graphics/Polygons/PolygonTwo';
 import { colors } from '@/styles/styles';
 import classes from '../WithMedia/styles.module.scss';
+import { Cell, Grid } from '@faceless-ui/css-grid';
 
 type Props = {
   content: unknown;
@@ -14,10 +15,14 @@ const MinimalPageHero: React.FC<Props> = ({ content }) => {
   return (
     <div className={classes.minimalWrap}>
       <GridContainer>
-        <RichText
-          className={classes.richText}
-          content={content}
-        />
+        <Grid>
+          <Cell>
+            <RichText
+              className={classes.richText}
+              content={content}
+            />
+          </Cell>
+        </Grid>
       </GridContainer>
       <div className={classes.minimalBg}>
         <PolygonTwo
