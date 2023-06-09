300de0c7debc73ba1963ad22d155181b32a4c6d6
asb1512 <asb1512@gmail.com>
2023-06-09 11:49:13 -0400

    added scroll info provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import App from 'next/app';
 import { Poppins } from 'next/font/google';
 import { ModalContainer, ModalProvider } from '@faceless-ui/modal';
 import { WindowInfoProvider } from '@faceless-ui/window-info';
+import { ScrollInfoProvider } from '@faceless-ui/scroll-info';
 import { GridProvider } from '@faceless-ui/css-grid';
 import { breakpoints, zIndex, baseStyling } from '@/styles/styles';
 import type { FooterType, HeaderType, LegalType, SocialType } from '@/types/globals';
@@ -39,53 +40,55 @@ function MyApp(appProps: AppProps): React.ReactElement {
   } = appProps;
 
   return (
-    <WindowInfoProvider
-      breakpoints={{
-        xs: `(max-width: ${breakpoints.xs}px)`,
-        s: `(max-width: ${breakpoints.s}px)`,
-        m: `(max-width: ${breakpoints.m}px)`,
-        l: `(max-width: ${breakpoints.l}px)`,
-        xl: `(max-width: ${breakpoints.xl}px)`,
-      }}
-    >
-      <ModalProvider
-        zIndex={zIndex.modal}
-        classPrefix="rModal"
-        transTime={400}
+    <ScrollInfoProvider>
+      <WindowInfoProvider
+        breakpoints={{
+          xs: `(max-width: ${breakpoints.xs}px)`,
+          s: `(max-width: ${breakpoints.s}px)`,
+          m: `(max-width: ${breakpoints.m}px)`,
+          l: `(max-width: ${breakpoints.l}px)`,
+          xl: `(max-width: ${breakpoints.xl}px)`,
+        }}
       >
-        <GridProvider
-          rowGap={{
-            s: baseStyling.base(2),
-            m: baseStyling.base(2),
-            l: baseStyling.base(2),
-            xl: baseStyling.base(2),
-          }}
-          colGap={{
-            s: baseStyling.base(2),
-            m: baseStyling.base(2),
-            l: baseStyling.base(2),
-            xl: baseStyling.base(2),
-          }}
-          cols={{
-            s: 8,
-            m: 8,
-            l: 12,
-            xl: 12,
-          }}
+        <ModalProvider
+          zIndex={zIndex.modal}
+          classPrefix="rModal"
+          transTime={400}
         >
-          <div className={`${classes.app} ${poppins.className}`}>
-            <Header header={header} social={socialMedia} />
-            <Component
-              {...pageProps}
-              footer={footer}
-              socialMedia={socialMedia}
-              legal={legal}
-            />
-          </div>
-        </GridProvider>
-        <ModalContainer />
-      </ModalProvider>
-    </WindowInfoProvider>
+          <GridProvider
+            rowGap={{
+              s: baseStyling.base(2),
+              m: baseStyling.base(2),
+              l: baseStyling.base(2),
+              xl: baseStyling.base(2),
+            }}
+            colGap={{
+              s: baseStyling.base(2),
+              m: baseStyling.base(2),
+              l: baseStyling.base(2),
+              xl: baseStyling.base(2),
+            }}
+            cols={{
+              s: 8,
+              m: 8,
+              l: 12,
+              xl: 12,
+            }}
+          >
+            <div className={`${classes.app} ${poppins.className}`}>
+              <Header header={header} social={socialMedia} />
+              <Component
+                {...pageProps}
+                footer={footer}
+                socialMedia={socialMedia}
+                legal={legal}
+              />
+            </div>
+          </GridProvider>
+          <ModalContainer />
+        </ModalProvider>
+      </WindowInfoProvider>
+    </ScrollInfoProvider>
   );
 }
 
