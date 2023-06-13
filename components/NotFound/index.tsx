467044682a0116e467303a152576ca454313ceb5
asb1512 <asb1512@gmail.com>
2023-06-13 11:25:40 -0400

    restyled 404 error

--- components/NotFound/index.tsx
+++ components/NotFound/index.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import classes from './styles.module.scss';
+import styles from '../../styles/app.module.scss';
 
-const NotFound: React.FC = () => <h1>404 - Page Not Found</h1>;
+const NotFound: React.FC = () => (
+  <div className={classes.wrapper}>
+    <div>
+      <h1 className={styles.h1}>404</h1>
+      <h4 className={styles.h4} style={{ color: 'white' }}>Page not found</h4>
+    </div>
+  </div>
+);
 
 export default NotFound;
