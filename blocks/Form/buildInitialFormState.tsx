e2dce30da810b7a50ac77297af9aa0019d585625
asb1512 <andrew@olympicmedia.com>
2023-06-21 02:52:49 -0400

    making typesafe

--- blocks/Form/buildInitialFormState.tsx
+++ blocks/Form/buildInitialFormState.tsx
@@ -1,38 +1,38 @@
 import type { FormFieldBlock } from './types';
 
 export const buildInitialFormState = (fields: FormFieldBlock[]) => {
-  return fields.reduce((initialSchema, field) => {
-    if (field.blockType === 'checkbox') {
+  return fields.reduce((initialSchema: any, field) => {
+    if (field.blockType === 'checkbox' && field.blockName) {
       return {
         ...initialSchema,
         [field.blockName]: false,
       }
     }
-    if (field.blockType === 'country') {
+    if (field.blockType === 'country' && field.blockName) {
       return {
         ...initialSchema,
         [field.blockName]: '',
       }
     }
-    if (field.blockType === 'email') {
+    if (field.blockType === 'email' && field.blockName) {
       return {
         ...initialSchema,
         [field.blockName]: '',
       }
     }
-    if (field.blockType === 'text') {
+    if (field.blockType === 'text' && field.blockName) {
       return {
         ...initialSchema,
         [field.blockName]: '',
       }
     }
-    if (field.blockType === 'select') {
+    if (field.blockType === 'select' && field.blockName) {
       return {
         ...initialSchema,
         [field.blockName]: '',
       }
     }
-    if (field.blockType === 'state') {
+    if (field.blockType === 'state' && field.blockName) {
       return {
         ...initialSchema,
         [field.blockName]: '',
