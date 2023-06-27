aa18b0ee83f76d95486b41a29947670c72d65fe2
asb1512 <asb1512@gmail.com>
2023-06-27 17:29:41 -0400

    Twitter Feed block build out

--- blocks/TwitterFeed/index.tsx
+++ blocks/TwitterFeed/index.tsx
@@ -3,6 +3,7 @@ import Script from 'next/script';
 import type { TwitterFeedBlockType } from '@/types/blocks';
 import GridContainer from '@/components/layout/GridContainer';
 import { Grid, Cell } from '@faceless-ui/css-grid';
+import RichText from '@/components/RichText';
 import classes from './styles.module.scss';
 import { colors } from '@/styles/styles';
 
@@ -17,6 +18,11 @@ const TwitterFeedBlock: React.FC<Props> = ({
     className={classes.wrapper}
     style={{ backgroundColor: colors[backgroundColor] }}
   >
+    {
+      richText && (
+        <RichText content={richText} />
+      )
+    }
     <GridContainer>
       <Grid>
         <Cell
@@ -29,7 +35,13 @@ const TwitterFeedBlock: React.FC<Props> = ({
           colsS={8}
           startS={1}
         >
-          <a className="twitter-timeline" href="https://twitter.com/TaylerRahm2024?ref_src=twsrc%5Etfw">Tweets by TaylerRahm2024</a>
+          <a
+            className={`twitter-timeline ${classes.link}`}
+            href="https://twitter.com/TaylerRahm2024?ref_src=twsrc%5Etfw"
+            style={{ color: colors['yellow'] }}
+          >
+            Tweets by TaylerRahm2024
+          </a>
           <Script src="https://platform.twitter.com/widgets.js" />
         </Cell>
       </Grid>
